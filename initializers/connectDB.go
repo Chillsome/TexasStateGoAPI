bffa98c9fae17a4d63ea634163a93d04cd060a07
Chillsome <mojica.david123@gmail.com>
2024-01-08 09:27:50 -0600

    Added panic

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -17,6 +17,7 @@ func ConnectDB(config *Config) {
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
+		panic("Hello")
 	}
 	fmt.Println("? Connected Successfully to the Database")
 }
